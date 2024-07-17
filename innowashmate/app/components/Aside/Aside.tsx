3a3b3b20dab97b78a9fa7be552102a67db3bb4de
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-17 20:39:26 +0300

    feat: add component, styles, put to aside

--- innowashmate/app/components/Aside/Aside.tsx
+++ innowashmate/app/components/Aside/Aside.tsx
@@ -5,10 +5,12 @@ import {Link} from 'react-router-dom';
 import NavBarItems from '../NavBarItems/NavBarItems';
 
 import './aside.css'
+import UserInfo from "@/app/components/UserInfo/UserInfo";
 
 const Aside: React.FC = () => {
     return (
         <div className='sidebar'>
+            <UserInfo  />
             <NavBarItems/>
             <Image src={photo} alt="Logo" width={320.85} height={200} className='logo'/>
         </div>
