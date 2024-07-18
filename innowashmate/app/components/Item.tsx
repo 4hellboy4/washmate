58be027adf7044bb7d58adcbd70fb3cbaf634acf
Baha Alimi <a.alimi@innopolis.university>
2024-07-18 04:39:26 +0300

    Implementation of item details page

---
+++ innowashmate/app/components/Item.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Image from 'next/image';
+import './item.css'
+import clock from '../assets/clock.svg';
+const Item: React.FC = () => {
+    return (
+        <div className='home'>
+            <div className='itemdetails'>
+                <div className='details'>
+                    <div className='bookbox'>
+                        <Image src={clock} alt='clock' className='clock'></Image>
+                        <div className='machinecont'>
+                            <h1 className='machine'>Dryer 1</h1>
+                            <p className='time'>Time Left: 60 min</p>
+                        </div>
+                    </div>
+                    <div className='btnn'>
+                        <h1 className='building'>Dorm 7</h1>
+                    </div>
+                    <div className='btnn'>
+                        <h1 className='building'>Floor 11</h1>
+                    </div>
+                </div>
+                <div className='dboxcont'>
+                    <div className='dbox'>
+                        <h1 className='dboxt'>15:00 - 14:00</h1>
+                    </div>
+                    <div className='dbox'>
+                        <h1 className='dboxt'>60 minutes</h1>
+                    </div>
+                    <div className='dbox'>
+                        <h1 className='dboxt'>60 min 00 secs</h1>
+                    </div>
+                </div>
+                <div className='cancelbtn'>
+                    <h1>CANCEL</h1>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Item;
--- innowashmate/app/pages/App.tsx
+++ innowashmate/app/pages/App.tsx
@@ -4,6 +4,7 @@ import Header from "../components/Header"
 import Home from "../components/Home"
 import About from "../components/About"
 import Dashboard from "../components/Dashboard"
+import Item from "../components/Item"
 import './app.css'
 
 const App: React.FC = () => {
