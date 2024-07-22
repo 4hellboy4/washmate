5f1e93f53a497dd397f990f94db5aa30a9e60798
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-22 10:05:33 +0300

    feat: add logout

--- innowashmate/app/components/NavBar/NavBar.tsx
+++ innowashmate/app/components/NavBar/NavBar.tsx
@@ -7,13 +7,28 @@ import info from '../../assets/info.svg';
 import user from '../../assets/user.svg';
 import smile from '../../assets/smile.svg';
 import telegram from '../../assets/telegram.svg';
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+
+import { signOut } from 'firebase/auth';
+import { FIREBASE_AUTH } from '@/FirebaseConfig'; // Adjust the path according to your project structure
 
 import PersonalInfo from './PersonalInfo/PersonalInfo';
 
 import './NavBar.css';
 
 const NavBar: React.FC = () => {
+  const navigate = useNavigate();
+
+
+  const handleLogout = async () => {
+    try {
+      await signOut(FIREBASE_AUTH);
+      navigate('/login'); // Redirect to the login page after logout
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
   return (
     <main>
       <div className="sidebar">
@@ -43,19 +58,19 @@ const NavBar: React.FC = () => {
           <Link to={'/login'}>
             <div className="button">
               <Image src={info} alt="info" className="icon"></Image>
-              <h6 className="servicef">login</h6>
+              <h6 className="servicef">Login</h6>
             </div>
           </Link>
           <Link to={'/signup'}>
             <div className="button">
               <Image src={info} alt="info" className="icon"></Image>
-              <h6 className="servicef">signup</h6>
+              <h6 className="servicef">Signup</h6>
             </div>
           </Link>
-          {/*<div className='logout'>*/}
-          {/*  <Image src={user} alt='logout' className='usericon'></Image>*/}
-          {/*  <h6 className='servicef'>Logout</h6>*/}
-          {/*</div>*/}
+          <div className='logout' onClick={handleLogout}>
+            <Image src={user} alt='logout' className='usericon'></Image>
+            <h6 className='servicef'>Logout</h6>
+          </div>
         </div>
         <Image src={photo} alt="logo" className="logo"></Image>
       </div>
